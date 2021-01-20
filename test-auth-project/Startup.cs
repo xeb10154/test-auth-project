627016adff45b0a8d0cba2a436c712e3ac73b3ec
raymond yau <raymond.yau@swipiicard.com>
2021-01-20 13:09:44 +0000

    Added JwtBearer Package and added Auth0 Startup.cs setup code

--- test-auth-project/Startup.cs
+++ test-auth-project/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,17 @@ namespace test_auth_project
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Add Authentication Services
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = "https://dev-6u8b8k1u.eu.auth0.com/";
+                options.Audience = "weather-api";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +58,10 @@ namespace test_auth_project
             {
                 endpoints.MapControllers();
             });
+
+            // Enable authentication middleware
+            app.UseAuthentication();
+
         }
     }
 }
